75fa7e293e01ec5749fe4af284d6c3277a300f9a
Sam Page <samnpage96@gmail.com>
2024-04-17 17:48:57 -0400

    Updated dropdown to be removed when user scrolls up

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 let lastScrollTop = 0;
 const navbar = document.querySelector(".nav");
+const navList = document.querySelector(".nav-list");
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-    if (scrollTop < lastScrollTop) {
-        // Scrolling up
-        navbar.classList.add('nav-fixed');
-    } else {
-        // Scrolling down
-        navbar.classList.remove('nav-fixed');
-    }
-
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+window.addEventListener('scroll', () => {
+    showDropdown();
 });
 
 
@@ -20,7 +11,26 @@ const toggleNavbar = () => {
     const navbarList = document.getElementById("navList");
     if (navbarList.style.display === "block") {
         navbarList.style.display = "none";
-    }   else {
+    } else {
         navbarList.style.display = "block";
     }
+}
+
+const showDropdown = () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop < lastScrollTop) {
+        // Scrolling up
+        navbar.classList.add('nav-fixed');
+
+        // Check if navbar list is open and scrolling up, then close it
+        if (navList.style.display === "block") {
+            toggleNavbar();
+        }
+    } else {
+        // Scrolling down
+        navbar.classList.remove('nav-fixed');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
 }
